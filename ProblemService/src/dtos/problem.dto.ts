190cf38930bdcbd3fbe5c72c126a52b658a03a99
ayush <rajput.ayush8jn@gmail.com>
2025-08-28 08:49:15 +0530

    added the dtos

---
+++ ProblemService/src/dtos/problem.dto.ts
@@ -0,0 +1,19 @@
+import { ITestCase } from "../models/problem.model";
+
+export interface CreateProblemDto {
+	title: string;
+	description: string;
+	difficulty: 'easy' | 'medium' | 'hard';
+	editorial?: string;
+	testCases: ITestCase[];
+}
+
+export interface UpdateProblemDto {
+	title?: string;
+	description?: string;
+	difficulty?: 'easy' | 'medium' | 'hard';
+	editorial?: string;
+	testCases?: ITestCase[];
+}
+
+
